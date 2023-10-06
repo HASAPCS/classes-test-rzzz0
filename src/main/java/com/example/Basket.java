74b6208bf6135730ce5c4548639622b8fae169c7
rzzz0 <rudyhsurh@gmail.com>
2023-10-06 13:30:44 +0800

    test

--- src/main/java/com/example/Basket.java
+++ src/main/java/com/example/Basket.java
@@ -8,15 +8,39 @@ public class Basket {
 
     public Basket(int capacity) {
         // TODO: Initialize the attributes (and initialize the fruits list)
+        this.capacity = capacity;
+        this.fruits = new ArrayList<Fruit>();
     }
 
     public void addFruit(Fruit fruit) {
         // TODO: Add the fruit to the basket IF THERE IS SPACE!
+        if (this.fruits.size() < this.capacity){
+            this.fruits.add(fruit);
+        }
+       
     }
 
     public void removeFruit(Fruit fruit) {
         // TODO: Remove the fruit from the basket
+        removeFruit(fruit);
     }
 
     // TODO: Implement getters, setters, and any other methods you find necessary
+    public int getCapacity(){
+        return this.capacity;
+    }
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+
+    public void setFruits(Fruit fruits){
+        this.fruits = Fruit fruits;
+
+    }
+    public String getFruits(){
+        return fruits;
+    }
 }
+
+
+
--- src/main/java/com/example/Fruit.java
+++ src/main/java/com/example/Fruit.java
@@ -8,7 +8,39 @@ public class Fruit {
 
     public Fruit(String name, String color, String taste, double weight) {
         // TODO: Initialize the attributes
+        this.name = name;
+        this.color = color;
+        this.taste = taste;
+        this.weight = weight;
+
     }
 
     // TODO: Implement getters, setters, and any other methods you find necessary
+    public String getName(){
+        return this.name;
+    }
+    public String getColor(){
+        return this.color;
+    }
+    public String getTaste(){
+        return this.taste;
+    }
+    public Double getWeight(){
+        return this.weight;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setColor(String color){
+        this.color=color;
+    }
+    public void setTaste(String taste){
+        this.taste = taste;
+
+    }
+    public void setWeight(Double weight){
+        this.weight = weight;
+
+    }
 }
